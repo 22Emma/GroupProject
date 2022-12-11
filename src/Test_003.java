f0777a42513cdd9199725fa89cdb8d6bc1f9ce17
22Emma <117785168+22Emma@users.noreply.github.com>
2022-12-11 13:15:30 +0000

    This is the code I wrote from github

--- src/Test_003.java
+++ src/Test_003.java
@@ -1,5 +1,25 @@
 public class Test_003 {
     public static void main(String[] args) {
         // this is my third class
+        // i am doing this edit from github
+        // this is my third line
+        
+        String word="Hi, how are you";
+        System.out.println(word);
+        
+        int a=10;
+        int b=23;
+        String result;
+        
+        if(a>b){
+          result=(a+" is bigger than "+b);
+        }else{
+          result=(b+" is bigger than "+a);
+        }
+        System.out.println(result);
+        
+        
+        
+        
     }
 }
